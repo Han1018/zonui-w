24624ccc43e0c86129d4c15bb93b01e223afc540
Han1018 <han1018@example.com>
2026-01-02 09:17:01 +0800

    fix: correct base path and resource references for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 針對 https://han1018.github.io/zonui-website/ 的部署設定
-  base: '/zonui-website/', 
+  // 針對 https://han1018.github.io/zonui-w/ 的部署設定
+  base: '/zonui-w/', 
   build: {
     outDir: 'dist',
     sourcemap: false
